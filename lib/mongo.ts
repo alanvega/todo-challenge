2119e67e7351edb6f7be84e22dfe0780b60256ca
alanvega <alvega@despegar.com>
2023-04-16 23:50:57 -0300

    Added atlas mongodb user, password and server

--- lib/mongo.ts
+++ lib/mongo.ts
@@ -1,7 +1,11 @@
 import {MongoClient} from 'mongodb';
 
+// const uri: string = "mongodb://localhost:27017/todos"; //only for local
+
 // this should be moved to a config file on a real app
-const uri: string = "mongodb://localhost:27017/todos";
+const user = 'user';
+const password = 'SYe2OKhUZS1qOBcd';
+const uri: string = `mongodb+srv://${user}:${password}@cluster0.mube2gl.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri);
 
